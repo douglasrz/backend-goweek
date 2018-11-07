72c5eef38725a9e666b896a3a1e968aaa11d6efe
Douglas <douglas@douglas-soft.local>
2018-11-07 01:58:42 -0300

    backend - goweek

---
+++ src/models/Tweet.js
@@ -0,0 +1,15 @@
+const mogoose = require("mongoose");
+
+const TweetSchema = new mogoose.Schema({
+    author: String,
+    content: String,
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,    
+    },
+});
+module.exports = mogoose.model('Tweet', TweetSchema);
